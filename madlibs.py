553e4d574d1582b1b1eb3e5ebfc8e2327420fb34
gloorya <gloriaalabi9@gmail.com>
2022-09-24 12:15:14 +0000

    first python game madlibs

---
+++ madlibs.py
@@ -0,0 +1,22 @@
+#This is a game that consists of one player prompting others for a list of words
+# to substitute for blanks in a story before reading aloud
+
+#string concatenation
+#some ways to do this
+
+#tiktok="LatechGloria"
+#print("follow me on {}".format(tiktok))
+#print(f"follow me on {tiktok}")
+adj = input('Adjective: ')
+verb1 = input('Verb: ')
+verb2 = input('Verb: ')
+verb3 = input('Verb: ')
+
+madlib = f"There are different types of secrets. \
+She had {verb1} onto plenty of them during her life, but this one was {adj}. She found herself {verb2} onto the worst type. \
+It was the type of secret that could gnaw away at your insides if you didn't tell someone about it,  \
+but it could end up getting you {verb3} if you did."
+
+print(madlib)
+
+#The End ;) LatechGloria
